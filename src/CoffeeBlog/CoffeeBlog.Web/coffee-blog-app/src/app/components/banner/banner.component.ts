f1fd847817dca5eaac35bd574c2cfb4a84f53955
Marco van Zuijlen <marco@chefdog.com>
2020-04-19 20:28:04 +0200

    working full screen banner

--- src/CoffeeBlog/CoffeeBlog.Web/coffee-blog-app/src/app/components/banner/banner.component.ts
+++ src/CoffeeBlog/CoffeeBlog.Web/coffee-blog-app/src/app/components/banner/banner.component.ts
@@ -11,13 +11,11 @@ export class BannerComponent implements OnInit {
 
   constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
-    config.interval = 10000;
-    config.wrap = false;
+    config.wrap = true;
     config.keyboard = false;
     config.pauseOnHover = false;
   }
 
   ngOnInit() {
   }
-
 }
